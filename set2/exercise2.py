d02ea6d726a20826dd73da2008a91a7a009e8535
Simon Byng <simon.byng@gmail.com>
2022-05-08 17:06:30 +1000

    Additional error removed

--- set2/exercise2.py
+++ set2/exercise2.py
@@ -18,7 +18,7 @@ def set2exersise2():
     wordArray = [getLetter(x) for x in indices]
     wordArray[0] = wordArray[0].upper()
     wordArray[1] = wordArray[1].upper()
-    wordArray[3] = wordArray[3].upper{}
+    wordArray[3] = wordArray[3].upper()
     secret_word="".join(wordArray)
     print(secret_word)
     return secret_word
